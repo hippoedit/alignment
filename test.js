a45a6bd45dac79c0a58c2f07642b0282aa0d29f3
HippoEDIT <alex@hippoedit.com>
2017-02-04 01:51:10 +0100

    Initial release

---
+++ test.js
@@ -0,0 +1,28 @@
+var x = 10.0;
+var y =20.0;
+var width = 10;
+var height = 100;
+
+var x = 10.0;
+var y 	  			=20.0;
+var widthaa = 100.0;
+var heightss = 	100.0;
+
+x =  10.0
+y =  20.0
+width =    100.0
+height = 100.0
+
+rectangle =
+    x: 10.0
+    y: 20.0
+    width: 100.0
+    height: 100.0
+
+.my-stylus-class
+    width 100px
+    height 100px
+    margin-left 10px
+    margin-right 10px
+    padding 5px
+  
